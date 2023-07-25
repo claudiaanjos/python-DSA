b056a041274df914cf0031d221e483fe8181488d
Claudia <ndosanjosc@gmail.com>
2023-07-25 10:56:46 -0300

    modulo 5

---
+++ modulos/modulo05/Lab/calculadora_v1.py
@@ -0,0 +1,38 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
+# A solução será apresentada no próximo capítulo!
+
+print("\n******************* Calculadora em Python *******************")
+
+print("Olá! Você está na calculadora em Python")
+print(""" 
+    Essas são nossas opções de funções:
+      [1] para adição;
+      [2] para subtração;
+      [3] para multiplicação;
+      [4] para divisão;
+      [0] para sair.
+""")
+      
+funcao = int(input("Digite a opção desejada: ")) 
+
+while funcao != 0:
+    
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
+
+    if funcao == 1:
+        print(f"{n1} + {n2} = {n1 + n2}")
+    elif funcao == 2:
+        print(f"{n1} - {n2} = {n1 - n2}")
+    elif funcao == 3:
+        print(f"{n1} x {n2} = {n1 * n2}")
+    elif funcao == 4:
+        print(f"{n1} / {n2} = {n1 / n2}")
+    else:
+        print("Opção inválida.")
+
+    funcao = int(input("Digite a opção desejada: "))
+
+print("Agradecemos por participar do teste da calculadora!")
---
+++ modulos/modulo05/readme.md
@@ -0,0 +1,5 @@
+# Módulo 5
+
+O módulo 5 traz as estruturas que permitem que a programação de computadores realmente seja efetiva: estruturas de repetição, estruturas condicionais e funções.
+
+Durante o módulo realizaremos uma série de atividades práticas com diversos exemplos.
