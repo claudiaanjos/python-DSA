a20bd24a462ce2ab311222d67801e9cdcef41957
Claudia <ndosanjosc@gmail.com>
2023-08-02 11:16:44 -0300

    modulo 8

---
+++ modulos/modulo08/Lab4/Lab4-v1.py
@@ -0,0 +1,83 @@
+# Hangman Game (Jogo da Forca) 
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Board (tabuleiro)
+board = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+# Classe
+class Hangman:
+
+	# Método Construtor
+
+	# Método para adivinhar a letra
+	
+	# Método para verificar se o jogo terminou
+		
+	# Método para verificar se o jogador venceu
+		
+	# Método para não mostrar a letra no board
+		
+	# Método para checar o status do game e imprimir o board na tela
+
